da0eca90e12698a4bc5a7f5109ca72a4164cd1c5
Yevhen Mazhulin <mazhulin474@gmail.com>
2020-11-02 20:26:35 +0200

    NEWS

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const News = (props) => {
-  return <div>News</div>;
+  return <div>
+
+<b>NEWS</b>
+<b>NEWS</b>
+<b>NEWS</b>
+<b>NEWS</b>
+<b>NEWS</b>
+  </div>;
 };
 
 export default News;
